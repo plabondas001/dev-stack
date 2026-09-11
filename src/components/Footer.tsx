ff033179cc3c051f99bc45c59b743a88a861ba7e
Plabondas001 <plabondas001@gmail.com>
2026-09-11 20:25:55 +0600

    fix: footer text size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
         <div className="flex items-start justify-between">
           <div className="space-y-5">
             <img className="w-44" src={footerLogo} alt="" />
-            <p className="font-semibold text-lg text-gray-400">
+            <p className="font-semibold text-gray-400">
               Curated tools, technologies, and resources for developers building{" "}
               <br />
               modern software.
@@ -16,19 +16,19 @@ const Footer = () => {
             <ul className="flex items-center gap-10 my-3">
               <li>
                 <a href="https://github.com/plabondas001">
-                  <FaGithub size={30} />
+                  <FaGithub size={25} />
                 </a>
               </li>
 
               <li>
                 <a href="https://x.com/">
-                  <FaTwitter size={30} />
+                  <FaTwitter size={25} />
                 </a>
               </li>
 
               <li>
                 <a href="https://www.linkedin.com/in/plabon-das-aab7b6378/">
-                  <FaLinkedin size={30} />
+                  <FaLinkedin size={25} />
                 </a>
               </li>
             </ul>
@@ -36,42 +36,42 @@ const Footer = () => {
 
           <div>
             <ul className="space-y-2">
-              <li className="font-semibold text-xl">PRODUCT</li>
-              <li className="text-lg text-gray-400">Home</li>
-              <li className="text-lg text-gray-400">Technologies</li>
-              <li className="text-lg text-gray-400">Projects</li>
+              <li className="font-semibold">PRODUCT</li>
+              <li className="text-gray-400">Home</li>
+              <li className="text-gray-400">Technologies</li>
+              <li className="text-gray-400">Projects</li>
             </ul>
           </div>
 
           <div>
             <ul className="space-y-2">
-              <li className="font-semibold text-xl">COMPANY</li>
-              <li className="text-lg text-gray-400">About</li>
-              <li className="text-lg text-gray-400">Contact</li>
-              <li className="text-lg text-gray-400">Careers</li>
+              <li className="font-semibold">COMPANY</li>
+              <li className="text-gray-400">About</li>
+              <li className="text-gray-400">Contact</li>
+              <li className="text-gray-400">Careers</li>
             </ul>
           </div>
 
           <div>
             <ul className="space-y-2">
-              <li className="font-semibold text-xl">LEGAL</li>
-              <li className="text-lg text-gray-400">Privacy Policy</li>
-              <li className="text-lg text-gray-400">Terms of Service</li>
+              <li className="font-semibold">LEGAL</li>
+              <li className="text-gray-400">Privacy Policy</li>
+              <li className="text-gray-400">Terms of Service</li>
             </ul>
           </div>
         </div>
         <div className="border-b border-gray-200 mt-10"></div>
 
         <div className="flex items-center justify-between mt-10">
-          <p className="text-xl text-gray-400">
+          <p className="text-gray-400">
             © 2026 Dev Stack. All rights reserved.
           </p>
           <div className="flex items-center gap-5">
-            <a href="/privacy" className="text-xl text-gray-400">
+            <a href="/privacy" className="text-gray-400">
               Privacy
             </a>
 
-            <a href="/terms" className="text-xl text-gray-400">
+            <a href="/terms" className="text-gray-400">
               Terms
             </a>
           </div>
